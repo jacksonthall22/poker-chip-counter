a355e60a4df5269940ee83bc0c6168b561d820d7
Jackson Hall <jacksonthall22@gmail.com>
2023-05-01 16:39:30 -0400

    Remove nonexistent component import

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
-import PokerChip from './components/PokerChip.vue'
 </script>
 
 <template>
